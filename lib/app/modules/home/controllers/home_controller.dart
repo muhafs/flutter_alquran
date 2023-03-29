62fd7a6c483c2e1982973d5eb2c24e6d4d7c1008
muhamadhaspin <muhamadhaspin@gmail.com>
2023-03-29 22:30:44 +0700

    - features:  create home page - features:  fetch all surah request

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,23 +1,26 @@
+// ignore_for_file: avoid_print
+
+import 'dart:convert';
+
+import 'package:flutter_alquran/app/data/models/surah_model.dart';
 import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
 
 class HomeController extends GetxController {
-  //TODO: Implement HomeController
+  Future<List<Surah>> getAllSurah() async {
+    Uri url = Uri.parse('https://al-quraan-api.vercel.app/surah');
 
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
+    var response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      List data = json.decode(response.body)['data'];
+
+      List<Surah> allSurah =
+          data.map((surah) => Surah.fromJson(surah)).toList();
+
+      return allSurah;
+    } else {
+      throw Exception('Somthing went wrong while fetching data');
+    }
   }
-
-  @override
-  void onReady() {
-    super.onReady();
-  }
-
-  @override
-  void onClose() {
-    super.onClose();
-  }
-
-  void increment() => count.value++;
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_alquran/app/data/models/surah_model.dart';
 
 import 'package:get/get.dart';
 
@@ -13,12 +14,37 @@ class HomeView extends GetView<HomeController> {
         title: const Text('HomeView'),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
-        ),
-      ),
+      body: FutureBuilder<List<Surah>>(
+          future: controller.getAllSurah(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+
+            if (!snapshot.hasData) {
+              return const Center(
+                child: Text('No Data Available'),
+              );
+            }
+
+            return ListView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, index) {
+                  Surah surah = snapshot.data![index];
+
+                  return ListTile(
+                    leading: CircleAvatar(
+                      child: Text('${surah.number}'),
+                    ),
+                    title: Text(surah.name.transliteration.en),
+                    subtitle: Text(
+                        '${surah.numberOfVerses} Ayah | ${surah.revelation.id}'),
+                    trailing: Text(surah.name.long),
+                  );
+                });
+          }),
     );
   }
 }
