04232d5d1e3455cb255b6569bf860e9dd04aa2c3
muhamadhaspin <muhamadhaspin@gmail.com>
2023-03-29 23:36:07 +0700

    - features: modify the list of all surah into gridView

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter_alquran/app/data/models/surah_model.dart';
 import 'package:flutter_alquran/app/routes/app_pages.dart';
 
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import '../controllers/home_controller.dart';
 
@@ -16,40 +17,79 @@ class HomeView extends GetView<HomeController> {
         centerTitle: true,
       ),
       body: FutureBuilder<List<Surah>>(
-          future: controller.getAllSurah(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
+        future: controller.getAllSurah(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
 
-            if (!snapshot.hasData) {
-              return const Center(
-                child: Text('No Data Available'),
-              );
-            }
+          if (!snapshot.hasData) {
+            return const Center(
+              child: Text('No Data Available'),
+            );
+          }
 
-            return ListView.builder(
-                itemCount: snapshot.data!.length,
-                itemBuilder: (context, index) {
-                  Surah surah = snapshot.data![index];
+          return GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+              childAspectRatio: 5 / 4,
+            ),
+            padding: const EdgeInsets.all(10),
+            itemCount: snapshot.data!.length,
+            itemBuilder: (context, index) {
+              Surah surah = snapshot.data![index];
 
-                  return ListTile(
-                    onTap: () => Get.toNamed(
-                      Routes.SURAH_DETAIL,
-                      arguments: surah,
+              return GestureDetector(
+                onTap: () => Get.toNamed(Routes.SURAH_DETAIL, arguments: surah),
+                child: Card(
+                  color: Colors.white70,
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.only(bottom: 12),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              CircleAvatar(
+                                child: Text('${surah.number}'),
+                              ),
+                              Text(surah.name.short),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.only(bottom: 12),
+                          child: Text(
+                            surah.name.transliteration.en,
+                            style: GoogleFonts.poppins(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text('${surah.numberOfVerses} Ayah'),
+                            Text(surah.revelation.id),
+                          ],
+                        ),
+                      ],
                     ),
-                    leading: CircleAvatar(
-                      child: Text('${surah.number}'),
-                    ),
-                    title: Text(surah.name.transliteration.en),
-                    subtitle: Text(
-                        '${surah.numberOfVerses} Ayah | ${surah.revelation.id}'),
-                    trailing: Text(surah.name.short),
-                  );
-                });
-          }),
+                  ),
+                ),
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
