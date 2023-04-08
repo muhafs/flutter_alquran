6ff760be3b1beeb7b1de4766ec1136438215f379
muhamadhaspin <muhamadhaspin@gmail.com>
2023-04-08 22:50:13 +0700

    - bugs: fix expanded error on surah detail page by removing expanded widget

--- lib/app/modules/surah_detail/views/surah_detail_view.dart
+++ lib/app/modules/surah_detail/views/surah_detail_view.dart
@@ -92,113 +92,111 @@ class SurahDetailView extends GetView<SurahDetailController> {
                 }
 
                 //? Loaded
-                return Expanded(
-                  child: ListView.builder(
-                    shrinkWrap: true,
-                    physics: const NeverScrollableScrollPhysics(),
-                    itemCount: surah.numberOfVerses,
-                    itemBuilder: (context, index) {
-                      detail.Verse? ayah = snapshot.data?.verses?[index];
+                return ListView.builder(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: surah.numberOfVerses,
+                  itemBuilder: (context, index) {
+                    detail.Verse? ayah = snapshot.data?.verses?[index];
 
-                      return Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          Container(
-                            margin: const EdgeInsets.only(bottom: 20),
-                            padding: const EdgeInsets.symmetric(
-                              horizontal: 10,
-                              vertical: 5,
-                            ),
-                            decoration: BoxDecoration(
-                              color: const Color(0xFF121931),
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Stack(
-                                  alignment: Alignment.center,
-                                  children: [
-                                    Image.asset(
-                                      'assets/images/icon_ayah.png',
-                                      width: 36,
-                                    ),
-                                    Text(
-                                      '${index + 1}',
-                                      style: GoogleFonts.poppins(
-                                        color: Colors.white,
-                                        fontSize: 12,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                                Row(
-                                  children: [
-                                    IconButton(
-                                      onPressed: () {},
-                                      icon: const Icon(
-                                        Icons.bookmark_border_outlined,
-                                        color: Color(0xFFA44AFF),
-                                      ),
-                                    ),
-                                    IconButton(
-                                      onPressed: () {},
-                                      icon: const Icon(
-                                        Icons.play_arrow,
-                                        color: Color(0xFFA44AFF),
-                                      ),
-                                    ),
-                                  ],
-                                )
-                              ],
-                            ),
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.only(bottom: 20),
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 10,
+                            vertical: 5,
                           ),
-                          Container(
-                            padding: const EdgeInsets.symmetric(horizontal: 10),
-                            margin: const EdgeInsets.only(bottom: 20),
-                            child: Text(
-                              ayah!.text?.arab ?? 'Something went error',
-                              style: GoogleFonts.amiri(
-                                color: Colors.white,
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
+                          decoration: BoxDecoration(
+                            color: const Color(0xFF121931),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Stack(
+                                alignment: Alignment.center,
+                                children: [
+                                  Image.asset(
+                                    'assets/images/icon_ayah.png',
+                                    width: 36,
+                                  ),
+                                  Text(
+                                    '${index + 1}',
+                                    style: GoogleFonts.poppins(
+                                      color: Colors.white,
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
                               ),
-                              textAlign: TextAlign.right,
+                              Row(
+                                children: [
+                                  IconButton(
+                                    onPressed: () {},
+                                    icon: const Icon(
+                                      Icons.bookmark_border_outlined,
+                                      color: Color(0xFFA44AFF),
+                                    ),
+                                  ),
+                                  IconButton(
+                                    onPressed: () {},
+                                    icon: const Icon(
+                                      Icons.play_arrow,
+                                      color: Color(0xFFA44AFF),
+                                    ),
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                        Container(
+                          padding: const EdgeInsets.symmetric(horizontal: 10),
+                          margin: const EdgeInsets.only(bottom: 20),
+                          child: Text(
+                            ayah!.text?.arab ?? 'Something went error',
+                            style: GoogleFonts.amiri(
+                              color: Colors.white,
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
                             ),
+                            textAlign: TextAlign.right,
                           ),
-                          Container(
-                            padding: const EdgeInsets.symmetric(horizontal: 10),
-                            margin: const EdgeInsets.only(bottom: 10),
-                            child: Text(
-                              ayah.translation?.en ?? 'Something went error',
-                              style: GoogleFonts.poppins(
-                                color: const Color(0xFFA19CC5),
-                                fontSize: 16,
-                                fontStyle: FontStyle.italic,
-                              ),
-                              textAlign: TextAlign.left,
+                        ),
+                        Container(
+                          padding: const EdgeInsets.symmetric(horizontal: 10),
+                          margin: const EdgeInsets.only(bottom: 10),
+                          child: Text(
+                            ayah.translation?.en ?? 'Something went error',
+                            style: GoogleFonts.poppins(
+                              color: const Color(0xFFA19CC5),
+                              fontSize: 16,
+                              fontStyle: FontStyle.italic,
                             ),
+                            textAlign: TextAlign.left,
                           ),
-                          const Divider(
-                            color: Color(0xFFA19CC5),
-                          ),
-                          Container(
-                            padding: const EdgeInsets.symmetric(horizontal: 10),
-                            margin: const EdgeInsets.only(bottom: 50),
-                            child: Text(
-                              ayah.translation?.id ?? 'Something went error',
-                              style: GoogleFonts.poppins(
-                                color: const Color(0xFFA19CC5),
-                                fontSize: 16,
-                              ),
-                              textAlign: TextAlign.justify,
+                        ),
+                        const Divider(
+                          color: Color(0xFFA19CC5),
+                        ),
+                        Container(
+                          padding: const EdgeInsets.symmetric(horizontal: 10),
+                          margin: const EdgeInsets.only(bottom: 50),
+                          child: Text(
+                            ayah.translation?.id ?? 'Something went error',
+                            style: GoogleFonts.poppins(
+                              color: const Color(0xFFA19CC5),
+                              fontSize: 16,
                             ),
+                            textAlign: TextAlign.justify,
                           ),
-                        ],
-                      );
-                    },
-                  ),
+                        ),
+                      ],
+                    );
+                  },
                 );
               },
             ),
